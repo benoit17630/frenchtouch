633f00f4f151bd8753538c7d0862da3e8c1986f4
benoit17630 <benoit.menier@orange.fr>
2022-05-25 12:43:26 +0200

    add

---
+++ src/Entity/User.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private ?string $username;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private string $password;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Don::class )]
+    private $dons;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $commentaire;
+
+    #[ORM\Column(type: 'float')]
+    private int $taxe = 15;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $avatar;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $locale;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Envoye::class)]
+    private $envoyes;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isBanque = false;
+
+    public function __construct()
+    {
+        $this->dons = new ArrayCollection();
+        $this->envoyes = new ArrayCollection();
+    }
+    public function __toString(): string
+    {
+        return $this->username;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, Don>
+     */
+    public function getDons(): Collection
+    {
+        return $this->dons;
+    }
+
+    public function addDon(Don $don): self
+    {
+        if (!$this->dons->contains($don)) {
+            $this->dons[] = $don;
+            $don->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDon(Don $don): self
+    {
+        if ($this->dons->removeElement($don)) {
+            // set the owning side to null (unless already changed)
+            if ($don->getUser() === $this) {
+                $don->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getTaxe(): ?float
+    {
+        return $this->taxe;
+    }
+
+    public function setTaxe(float $taxe): self
+    {
+        $this->taxe = $taxe;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(?string $avatar): self
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getAvatarUrl(): ?string
+    {
+        if (!$this->avatar) {
+            return null;
+        }
+
+        if (strpos($this->avatar, '/') !== false) {
+            return $this->avatar;
+        }
+
+        return sprintf('/uploads/avatars/%s', $this->avatar);
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(?string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Envoye>
+     */
+    public function getEnvoyes(): Collection
+    {
+        return $this->envoyes;
+    }
+
+    public function addEnvoye(Envoye $envoye): self
+    {
+        if (!$this->envoyes->contains($envoye)) {
+            $this->envoyes[] = $envoye;
+            $envoye->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEnvoye(Envoye $envoye): self
+    {
+        if ($this->envoyes->removeElement($envoye)) {
+            // set the owning side to null (unless already changed)
+            if ($envoye->getUser() === $this) {
+                $envoye->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function isIsBanque(): ?bool
+    {
+        return $this->isBanque;
+    }
+
+    public function setIsBanque(bool $isBanque): self
+    {
+        $this->isBanque = $isBanque;
+
+        return $this;
+    }
+}
